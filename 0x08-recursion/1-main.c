9f50bd86b22bcb3b0c30ef327d69e3591e71e783
Makuwe <mmakuwe98@gmail.com>
2023-03-09 04:35:09 -0800

    update

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -1,8 +1,12 @@
-#include <main.h>
+#include "main.h"
 
-/**main -  check the code
+/**
+ * main - check the code
  *
- *Return: always 0
-*/
-
+ * Return: Always 0.
+ */
 int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    return (0);
+}
