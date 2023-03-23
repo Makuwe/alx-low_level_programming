056daeb8bd14679994e5942ffc254efc41515645
Makuwe <mmakuwe98@gmail.com>
2023-03-23 13:06:07 +0200

    Update 2-main.c

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -1,1 +1,12 @@
-#include#include 
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(", ", 2, "Jay", "Django");
+    return (0);
+}
